780afac605219eb78c42e29b7976df32d529db0b
Anastasia Omelchenko <fowesn@bk.ru>
2023-05-15 19:16:19 +0300

    [#12] Added base components

---
+++ src/app/core/models/index.ts
@@ -0,0 +1,5 @@
+export * from './BaseColor';
+export * from './DIalog';
+export * from './InputType';
+export * from './Postition';
+export * from './Snackbar';
---
+++ src/app/modules/shared/directives/BaseInputComponent.ts
@@ -0,0 +1,38 @@
+import {
+  Directive,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { EInputType } from 'src/app/core/models';
+
+@Directive()
+export class BaseInputComponent {
+  @Input() disabled = false;
+  @Input() inputControl = new FormControl();
+  @Input() label: string;
+  @Input() placeholder: string;
+  @Input() type = EInputType.text;
+
+  @Output() blur = new EventEmitter<void>();
+  @Output() focus = new EventEmitter<void>();
+
+  focused = false;
+
+  get showError(): boolean {
+    return !this.focused &&
+      this.inputControl.touched &&
+      this.inputControl.invalid;
+  }
+
+  onBlur(): void {
+    this.focused = false;
+    this.blur.emit();
+  }
+
+  onFocus(): void {
+    this.focused = true;
+    this.focus.emit();
+  }
+}
