822b72fffd72247d657769f9c19f0efff55b0e52
Anastasia Omelchenko <fowesn@bk.ru>
2023-07-19 16:26:41 +0300

    [#42] Added account & profile services

--- src/app/modules/profile/components/profile-card/profile-card.component.ts
+++ src/app/modules/profile/components/profile-card/profile-card.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { IProfile } from 'src/app/core/models';
 
 @Component({
   selector: 'app-profile-card',
   templateUrl: './profile-card.component.html',
   styleUrls: ['./profile-card.component.scss']
 })
-export class ProfileCardComponent { }
+export class ProfileCardComponent {
+  @Input() profile: IProfile;
+}
