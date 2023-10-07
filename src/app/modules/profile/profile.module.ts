a2c46b191a10196274862a70adaf7b5a0d384963
Anastasia Omelchenko <fowesn@bk.ru>
2023-10-07 15:57:12 +0300

    [#54] Refactored shared module

--- src/app/modules/profile/profile.module.ts
+++ src/app/modules/profile/profile.module.ts
@@ -5,6 +5,7 @@ import { SharedModule } from 'src/app/modules/shared/shared.module';
 
 import { ProfileBlockComponent } from './components/profile-block/profile-block.component';
 import { ProfileCardComponent } from './components/profile-card/profile-card.component';
+import { ProfileSettingsComponent } from './components/profile-settings/profile-settings.component';
 import { WishFormComponent } from './components/wish-form/wish-form.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { ProfileRoutingModule } from './profile-routing.module';
@@ -14,6 +15,7 @@ import { ProfileRoutingModule } from './profile-routing.module';
     ProfileBlockComponent,
     ProfileCardComponent,
     ProfileComponent,
+    ProfileSettingsComponent,
     WishFormComponent,
   ],
   imports: [
