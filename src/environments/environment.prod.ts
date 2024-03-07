21a48112eff96d7cccf9461f8586f43524fdcbb3
Anastasia Omelchenko <fowesn@bk.ru>
2024-03-07 15:59:45 +0300

    [#146] Исправлено формирование URL для обращений к API

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,8 @@
 export const environment = {
+  bff: {
+    host: `${window.location.protocol}//${window.location.hostname}`,
+    port: 8000,
+    url: '',
+  },
   production: true,
-  bffUrl: '',
 };
