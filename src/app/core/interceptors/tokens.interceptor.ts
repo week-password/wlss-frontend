bbdc470b4aadadb4d598c072d79455a587fcaffd
Anastasia Omelchenko <fowesn@bk.ru>
2024-03-16 17:37:38 +0300

    [#166] Исправлена отправка нескольких запросов на обновление токенов в случае с параллельными вызовами API методов при истёкшем `access token`

--- src/app/core/interceptors/tokens.interceptor.ts
+++ src/app/core/interceptors/tokens.interceptor.ts
@@ -1,9 +1,10 @@
 import { HttpContextToken, HttpErrorResponse, HttpEvent, HttpHandlerFn, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { Router } from '@angular/router';
-import { catchError, Observable, switchMap, throwError } from 'rxjs';
+import { catchError, filter, first, Observable, switchMap, throwError } from 'rxjs';
 
 import { AuthService } from '@auth/services/client';
+import { SessionStateService } from '@auth/services/state';
 import { EHttpError } from '@core/models/api';
 
 export const refreshRequestTokens = new HttpContextToken(() => true);
@@ -11,6 +12,7 @@ export const refreshRequestTokens = new HttpContextToken(() => true);
 export const tokensInterceptor: HttpInterceptorFn = (request: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> => {
   const authService = inject(AuthService);
   const router = inject(Router);
+  const sessionStateService = inject(SessionStateService);
 
   return next(request).pipe(
     catchError((error: HttpErrorResponse) => {
@@ -20,9 +22,21 @@ export const tokensInterceptor: HttpInterceptorFn = (request: HttpRequest<unknow
       if (request.context.get(refreshRequestTokens) === false) {
         return throwError(() => error);
       }
+      if (sessionStateService.refreshingInProgress.value) {
+        return sessionStateService.refreshingInProgress.pipe(
+          filter((refreshingInProgress: boolean) => !refreshingInProgress),
+          first(),
+          switchMap(() => next(request)),
+        );
+      }
+      sessionStateService.setRefreshingInProgress(true);
       return authService.refreshTokens().pipe(
-        switchMap(() => next(request)),
+        switchMap(() => {
+          sessionStateService.setRefreshingInProgress(false);
+          return next(request);
+        }),
         catchError((error: HttpErrorResponse) => {
+          sessionStateService.setRefreshingInProgress(false);
           router.navigate(['signin']);
           return throwError(() => error);
         }),
