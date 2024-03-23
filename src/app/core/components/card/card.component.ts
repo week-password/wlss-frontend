4b2aef4eb3b72820354201bd381d648421e6755c
Anastasia Omelchenko <fowesn@bk.ru>
2024-03-23 19:40:14 +0300

    [#206] Изменено ограничение на максимальную длину описания желания

--- src/app/core/components/card/card.component.ts
+++ src/app/core/components/card/card.component.ts
@@ -1,12 +1,11 @@
 import { NgIf } from '@angular/common';
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 import { AvatarComponent } from '@core/components/avatar';
 import { EAvatarType } from '@core/models/client';
 
 const imports = [AvatarComponent, NgIf];
 @Component({
-  changeDetection: ChangeDetectionStrategy.OnPush,
   imports,
   selector: 'app-card',
   standalone: true,
@@ -21,4 +20,28 @@ export class CardComponent {
   @Input() headerBadge: string | null = null;
   @Input() presentationView = false;
   @Input() showControls: boolean;
+  @Input() visibleDescriptionLength: number;
+
+  showFullDescription = false;
+
+  get truncatedDescription(): string | null {
+    if (!this.description) {
+      return null;
+    }
+    if (!this.shouldTruncateDescription || this.showFullDescription) {
+      return this.description;
+    }
+    return this.description.slice(0, this.visibleDescriptionLength) + '...';
+  }
+
+  get shouldTruncateDescription(): boolean {
+    if (!this.description) {
+      return false;
+    }
+    return !!this.visibleDescriptionLength && this.description.length > this.visibleDescriptionLength;
+  }
+
+  switchDescriptionView(): void {
+    this.showFullDescription = !this.showFullDescription;
+  }
 }
