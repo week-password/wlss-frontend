41c4e644d399dc86ac0458ad4d51958530bdf137
Anastasia Omelchenko <fowesn@bk.ru>
2024-03-26 19:43:16 +0300

    [#212] Добавлена передача имени файла при загрузке

--- src/app/core/services/client/files.service.ts
+++ src/app/core/services/client/files.service.ts
@@ -6,12 +6,15 @@ import { FilesApiService } from '@core/services/api';
 
 @Injectable({ providedIn: 'root' })
 export class FilesService {
+  private readonly maxFileNameLength = 256;
+
   constructor(private readonly filesApiService: FilesApiService) { }
 
-  uploadFile(file: Blob): Observable<string> {
+  uploadFile(file: Blob, fileName: string): Observable<string> {
     const extension = file.type.split('/')[1];
+    const fileFullName = `${fileName.slice(0, this.maxFileNameLength - extension.length)}.${extension}`;
     const formData = new FormData();
-    formData.append('file', file, `file.${extension}`);
+    formData.append('file', file, fileFullName);
     return this.filesApiService.uploadFile(formData).pipe(
       switchMap((file: TUploadFileResponse) => of(file.id)),
     );
