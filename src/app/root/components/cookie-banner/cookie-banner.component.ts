665245c99cca4662e79e04972e2fd9bb6e364e08
Anastasia Omelchenko <fowesn@bk.ru>
2024-03-27 16:00:10 +0300

    [#218] Исправлено пропадание cookie-баннера при отображении ошибки

--- src/app/root/components/cookie-banner/cookie-banner.component.ts
+++ src/app/root/components/cookie-banner/cookie-banner.component.ts
@@ -1,20 +1,11 @@
-import { AfterViewInit, Component, TemplateRef, ViewChild } from '@angular/core';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { NgIf } from '@angular/common';
+import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { takeUntil } from 'rxjs';
+import { AngularSvgIconModule } from 'angular-svg-icon';
 
-import { BaseComponent } from '@core/base-components';
-import { SnackbarComponent } from '@core/components/snackbar';
-import {
-  EMatSnackbarHPosition,
-  EMatSnackbarVPosition,
-  EPosition,
-  ESnackbarView,
-  ETextPosition,
-  TSnackbarData,
-} from '@core/models/client';
+import { ButtonComponent } from '@core/components/button';
 
-const imports = [RouterLink];
+const imports = [AngularSvgIconModule, ButtonComponent, NgIf, RouterLink];
 @Component({
   imports,
   selector: 'app-cookie-banner',
@@ -22,43 +13,11 @@ const imports = [RouterLink];
   styleUrl: 'cookie-banner.component.scss',
   templateUrl: 'cookie-banner.component.html',
 })
-export class CookieBannerComponent extends BaseComponent implements AfterViewInit {
-  @ViewChild('cookieText') cookieText: TemplateRef<HTMLElement>;
+export class CookieBannerComponent {
+  dismissed = !!localStorage.getItem('cookie-banner-dismissed');
 
-  private readonly bannerTitle = 'Добро пожаловать в Wisher - сервис по исполнению желаний!';
-  private readonly bannerButtonText = 'Понятно';
-  private readonly bannerWidth = 350;
-
-  constructor(private readonly snackBar: MatSnackBar) {
-    super();
+  dismiss(): void {
+    this.dismissed = true;
+    window.localStorage.setItem('cookie-banner-dismissed', 'cookie-banner-dismissed');
   }
-
-  ngAfterViewInit(): void {
-    const cookieBannerDismissed = localStorage.getItem('cookie-banner-dismissed');
-    if (!cookieBannerDismissed) {
-      this.showCookieBanner();
-    }
-  }
-
-  private showCookieBanner(): void {
-    const data: TSnackbarData = {
-      textTemplate: this.cookieText,
-      title: this.bannerTitle,
-      buttonText: this.bannerButtonText,
-      width: this.bannerWidth,
-      catPosition: EPosition.bottom,
-      textAlign: ETextPosition.left,
-      view: ESnackbarView.info,
-    };
-    const cookieBannerRef = this.snackBar.openFromComponent(SnackbarComponent, {
-      data,
-      horizontalPosition: EMatSnackbarHPosition.end,
-      verticalPosition: EMatSnackbarVPosition.bottom,
-    });
-
-    cookieBannerRef.afterDismissed().pipe(takeUntil(this.destroy$)).subscribe(() => {
-      window.localStorage.setItem('cookie-banner-dismissed', 'cookie-banner-dismissed');
-    });
-  }
-
 }
